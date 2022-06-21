c7b975fc3f6e7df2a799e9113370bf283261d6d2
FRANCO1357 <fmelani13@gmail.com>
2022-06-21 15:52:21 +0200

    added calc

--- js/scipt.js
+++ js/scipt.js
@@ -6,3 +6,30 @@
 // 6. comunicare il prezzo del biglietto
 
 
+const km = parseInt(prompt('quanti chilometri devi percorrere?', '10'));
+console.log(km);
+
+const age = parseInt(prompt('quanti anni hai?', '20'));
+console.log(age);
+
+const price = (km * 0.21);
+console.log(price);
+
+// VALIDAZIONE
+
+if (isNaN(km) || isNaN(age)){
+    alert('I valori inseriti non sono corretti')
+} else {
+
+if (age < 18){
+    const finalPrice = price / 100 * 80;
+    console.log(finalPrice);    
+} else if (age > 65) {
+    const finalPrice = price / 100 * 60;
+    console.log(finalPrice);  
+} else {
+    const finalPrice = price;
+    console.log(finalPrice);  
+}
+
+}
