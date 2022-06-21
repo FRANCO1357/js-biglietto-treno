b1eff13e770ea430dd43a9ff515c6a43463d65d5
FRANCO1357 <fmelani13@gmail.com>
2022-06-21 15:08:37 +0200

    added boilerplate

---
+++ js/scipt.js
@@ -0,0 +1,8 @@
+// 1. chiedere i chilometri da percorrere
+// 2. chiedere l'età
+// 3. calcolare il prezzo del biglietto in base ai chilometri
+// 4. calcolare lo sconto in base all'età
+// 5. sottrarre lo sconto al prezzo del biglietto
+// 6. comunicare il prezzo del biglietto
+
+
